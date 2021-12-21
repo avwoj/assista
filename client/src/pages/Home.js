08705a5b2bca8b6b590c571e5728440a8d706e08
Alex Wojtyna <awojtyna@gmail.com>
2021-12-21 14:13:24 -0500

    first

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,9 @@
 import React from "react";
 import Login from "../components/Login";
-import TinyCalendar from "../components/TinyCalendar"
 
 function Home() {
   return (
     <div>
-      <h3>
-        This will be the home page! Include sign in/registration components
-        here.
-      </h3>
       <Login />
     </div>
   );
