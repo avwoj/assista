0e9ce1397b5b01831491668cfd3ef456906d7e03
Emily Chase <chase.emily@ymail.com>
2021-12-21 09:53:33 -0600

    styling for bootstrap added to html

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
 function Calendar() {
+  const [title, setTitle] = useState("");
   return (
     <div>
       <h3>Calendar will go here</h3>
+      <Button
+        onClick={() => {
+          setTitle("hello");
+          console.log(title);
+        }}
+      >
+        Change Title
+      </Button>
     </div>
   );
 }
