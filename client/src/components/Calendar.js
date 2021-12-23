cad2974f556b3c7d4d7a2a23f7145ac95b3c5968
Emily Chase <chase.emily@ymail.com>
2021-12-23 07:20:03 -0600

    add/delete modals added to calendar

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,22 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import FullCalendar, { formatDate } from "@fullcalendar/react"; //must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid"; //plugins
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 // https://fullcalendar.io/docs#toc
 
 function Calendar() {
-  let str = formatDate(new Date(), {
-    month: "long",
-    year: "numeric",
-    day: "numeric",
-  });
+  const [showAddEvent, setShowAddEvent] = useState(false);
+  const [showRemoveEvent, setShowRemoveEvent] = useState(false);
+  const [title, setTitle] = useState("");
+  const [eventInfo, setEventInfo] = useState(null);
+  const [deleteInfo, setDeleteInfo] = useState(null);
+
+  const handleCloseAddEvent = () => {
+    setShowAddEvent(false);
+    handleDateSelect();
+  };
+
+  const handleOpenAddEvent = (selectInfo) => {
+    setShowAddEvent(true);
+    setEventInfo(selectInfo);
+  };
+
+  const handleCloseRemoveEvent = () => {
+    setShowRemoveEvent(false);
+  };
+  const handleOpenRemoveEvent = (clickInfo) => {
+    setShowRemoveEvent(true);
+    setDeleteInfo(clickInfo);
+  };
+
+  // let str = formatDate(new Date(), {
+  //   month: "long",
+  //   year: "numeric",
+  //   day: "numeric",
+  // });
   // console.log(str);outputs current day, format December 21, 2021 (ex.)
 
-  const handleDateSelect = (selectInfo) => {
-    let title = prompt("Please enter a new title for your event");
-    let calendarApi = selectInfo.view.calendar;
+  const handleDateSelect = () => {
+    let calendarApi = eventInfo.view.calendar;
 
     calendarApi.unselect(); // clear date selection
 
@@ -24,41 +49,76 @@ function Calendar() {
       calendarApi.addEvent({
         // id: createEventId(), need a new function for id creation, will add in later
         title,
-        start: selectInfo.startStr,
-        end: selectInfo.endStr,
-        allDay: selectInfo.allDay,
+        start: eventInfo.startStr,
+        end: eventInfo.endStr,
+        allDay: eventInfo.allDay,
       });
     }
+    setTitle("");
   };
 
-  const handleEventClick = (clickInfo) => {
-    prompt(
-      `Are you sure you want to delete the event '${clickInfo.event.title}'`
-    );
-    console.log(clickInfo.event);
-    //need to update the functionality on this, just wanted to see if I could actually do it.
-    clickInfo.event.remove();
+  const deleteEvent = () => {
+    deleteInfo.event.remove();
+    handleCloseRemoveEvent();
   };
 
-  const [title, setTitle] = useState("");
   return (
-    <div>
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay",
-        }}
-        initialView="dayGridMonth"
-        editable={true}
-        selectable={true}
-        selectMirror={true} //draws a placeholder event in time view, I like it but we can scrap
-        dayMaxEvents={true} //prevents the calendar from resizing at month view if too many events
-        select={handleDateSelect}
-        eventClick={handleEventClick}
-      />
-    </div>
+    <>
+      <Modal show={showAddEvent} onHide={handleCloseAddEvent}>
+        <Modal.Header>
+          <Modal.Title>Add Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Enter the name of your event.
+          {
+            <input
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          }
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddEvent}>
+            Add Event
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showRemoveEvent} onHide={handleCloseRemoveEvent}>
+        <Modal.Header>
+          <Modal.Title>Remove Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to remove this event from your calendar?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={deleteEvent}>
+            Yes, remove it
+          </Button>
+          <Button variant="secondary" onClick={handleCloseRemoveEvent}>
+            No, I want to keep it
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <div style={{ position: "relative", zIndex: 0 }}>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true} //draws a placeholder event in time view, I like it but we can scrap
+          dayMaxEvents={true} //prevents the calendar from resizing at month view if too many events
+          select={handleOpenAddEvent}
+          eventClick={handleOpenRemoveEvent}
+        />
+      </div>
+    </>
   );
 }
 
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./navigation.css"
+import "./navigation.css";
 
 function Navigation() {
   return (
     <div className="nav">
       <h3>Navigation</h3>
-          <Link to="/">Home</Link>
-          <Link to="/user">User Page</Link>
-          <Link to="/calendar">Calendar</Link>
-          <Link to="/journal">Journal</Link>
+      <Link to="/">Home</Link>
+      {/* <Link to="/user">User Page</Link> */}
+      <Link to="/calendar">Calendar</Link>
+      <Link to="/journal">Journal</Link>
     </div>
   );
 }
