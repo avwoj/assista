588443a65cdec257a7be9bbde31bc8b2af2420f3
Alex Wojtyna <awojtyna@gmail.com>
2021-12-23 18:57:03 -0500

    fixed?

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -6,7 +6,6 @@ import interactionPlugin from "@fullcalendar/interaction";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-import "./calendar.css"
 // https://fullcalendar.io/docs#toc
 
 function Calendar() {
@@ -64,23 +63,62 @@ function Calendar() {
   };
 
   return (
-    <div className="fullCalendar">
-      <FullCalendar
-        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        headerToolbar={{
-          left: "prev,next today",
-          center: "title",
-          right: "dayGridMonth,timeGridWeek,timeGridDay",
-        }}
-        initialView="dayGridMonth"
-        editable={true}
-        selectable={true}
-        selectMirror={true} //draws a placeholder event in time view, I like it but we can scrap
-        dayMaxEvents={true} //prevents the calendar from resizing at month view if too many events
-        select={handleDateSelect}
-        eventClick={handleEventClick}
-      />
-    </div>
+    <>
+      <Modal show={showAddEvent} onHide={handleCloseAddEvent}>
+        <Modal.Header>
+          <Modal.Title>Add Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Enter the name of your event.
+          {
+            <input
+              placeholder="Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          }
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddEvent}>
+            Add Event
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showRemoveEvent} onHide={handleCloseRemoveEvent}>
+        <Modal.Header>
+          <Modal.Title>Remove Event</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Are you sure you want to remove this event from your calendar?
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={deleteEvent}>
+            Yes, remove it
+          </Button>
+          <Button variant="secondary" onClick={handleCloseRemoveEvent}>
+            No, I want to keep it
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <div style={{ position: "relative", zIndex: 0 }}>
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          headerToolbar={{
+            left: "prev,next today",
+            center: "title",
+            right: "dayGridMonth,timeGridWeek,timeGridDay",
+          }}
+          initialView="dayGridMonth"
+          editable={true}
+          selectable={true}
+          selectMirror={true} //draws a placeholder event in time view, I like it but we can scrap
+          dayMaxEvents={true} //prevents the calendar from resizing at month view if too many events
+          select={handleOpenAddEvent}
+          eventClick={handleOpenRemoveEvent}
+        />
+      </div>
+    </>
   );
 }
 
